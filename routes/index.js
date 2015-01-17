f1d282be872f29d221b00be8b7a3621802e814c6
Anthony Calandra <anthony@anthony-calandra.com>
2015-01-17 17:31:34 -0500

    Added delete functionality. Code refactoring.

--- routes/index.js
+++ routes/index.js
@@ -1,20 +1,6 @@
 var Post = require('../models/post.js');
 module.exports = function(app) {
-  app.get('/', function(req, res) {
-    // Get all posts in descending order by date.
-    Post.find().sort({
-      'date': 'desc'
-    }).exec(function(err, posts) {
-      var context = {
-        posts: posts.map(function(post) {
-          return {
-            title: post.title,
-            urlTitle: post.urlTitle,
-            content: post.getPreviewContent()
-          };
-        })
-      };
-      res.render('home', context);
-    });
-  });
+	app.get('/', function(req, res) {
+		res.render('home');
+	});
 };
