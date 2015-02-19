d415a730f703f9375bc17fe79d5a85f149e2ea7d
Anthony Calandra <anthony@anthony-calandra.com>
2015-02-19 16:58:25 -0500

    Responsive design added. Changes to template. New home page.

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var Post = require('../models/post.js');
 module.exports = function(app) {
-	app.get('/', function(req, res) {
-		res.render('home');
-	});
+  app.get('/', function(req, res) {
+    res.render('home', {
+      layout: false
+    });
+  });
 };
