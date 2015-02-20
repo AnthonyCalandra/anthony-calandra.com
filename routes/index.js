66e770b2bf9997db1cef6eac73b401bc5aee5cff
Anthony Calandra <anthony@anthony-calandra.com>
2015-02-19 23:52:21 -0500

    Dexter's lab.

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
 var Post = require('../models/post.js');
 module.exports = function(app) {
-  app.get('/', function(req, res) {
-    res.render('home', {
-      layout: false
-    });
-  });
+	app.get('/', function(req, res) {
+		res.render('home', {
+			layout: false
+		});
+	});
+
+	app.get('/dexterslab', function(req, res) {
+		res.render('dexterslab', {
+			layout: false
+		});
+	});
 };
